aa752c5e104b9b491b82365e1da0d29dd574ff08
lucg23 <lucasferreira.23m@gmail.com>
2021-06-26 00:26:05 -0300

    configurando responsividade da seções

--- src/components/Models/ModelOverlay/index.tsx
+++ src/components/Models/ModelOverlay/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import useWrapperScroll from '../useWrapperScroll';
+
 import { Container } from './styles';
 
 
 
 const ModelOverlay: React.FC = ({children}) => {
+  const {scrollY } = useWrapperScroll()
+  
+  
   return (
     <Container>
       <h1>{children}</h1>
